8820e74559aa125ed711f8b21d1621f12ca38db9
tuedoidoe <tuedoidoe@gmail.com>
2026-03-09 06:14:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Entradas Lay Away", page_icon="🤖", layout="wid
 def check_password():
     col1, col2, col3 = st.columns([1, 2, 1])
     with col1:
-        st.image("logo.png", width=400)
+        st.image("logo.png", width=350)
         def password_entered():
             if st.session_state["password"] == st.secrets["senha_secreta"]:
                 st.session_state["password_correct"] = True
