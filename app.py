6675a8ea472f50f062cc3592a8d6a47857deb8f9
tuedoidoe <tuedoidoe@gmail.com>
2026-03-09 11:10:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ if check_password():
                             # 3. Função para as linhas alternadas com centralização FORÇADA (!important)
                             def cores_alternadas(row):
                                 cor_fundo = '#4a4a4a' if row.name % 2 == 0 else '#333333'
-                                return [f'background-color: {cor_fundo}; color: white; text-align: center !important; font-size: 20px;' for _ in row]
+                                return [f'background-color: {cor_fundo}; color: white; text-align: center !important; font-size: 18px;' for _ in row]
 
                             # 4. Aplicando o Estilo Final
                             tabela_estilizada = tabela.style.apply(cores_alternadas, axis=1) \
