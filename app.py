7ab1df5f54fe331a9de2fab9070dc9ffdbe1dfa5
tuedoidoe <tuedoidoe@gmail.com>
2026-04-17 16:43:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ if check_password():
         df_bruto = st.session_state['df_bruto']
         
         # Grid para alinhar Resultados, Filtros, Ordenação e Botão de Exportar
-        col_res1, col_sort, col_ordem, col_odd, col_edge, col_btn = st.columns([4.3, 0.8, 0.8, 0.5, 0.5, 0.8])
+        col_res1, col_sort, col_ordem, col_odd, col_edge, col_btn = st.columns([4.4, 0.8, 0.8, 0.5, 0.5, 0.8])
         
         with col_sort:
             coluna_ordem = st.selectbox("Ordenar por", ["Horário", "EV+", "Score"])
