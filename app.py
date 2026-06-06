4cbff6d9ee8f0c136c5e1b537b9dfd669a900097
tuedoidoe <tuedoidoe@gmail.com>
2026-06-06 11:53:18 -0300

    Disable caching for carregar_mapeamentos function
    Commented out the cache decorator for the carregar_mapeamentos function to allow modifications to the mapeamentos.json file.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def drop_reset_index(df):
 # ==========================================
 # CARREGAMENTO DOS DICIONÁRIOS JSON
 # ==========================================
-# @st.cache_data
+@st.cache_data # Desativar, fazer as modificações no arquivo mapeamentos.json, rodar, ativar novamente e depois ativar.
 def carregar_mapeamentos():
     try:
         with open("mapeamentos.json", "r", encoding="utf-8") as f:
